ba86c1d59ea33ad9be2f46af186596e83e0b93d3
Yasir <63144490+myawesomehub@users.noreply.github.com>
2025-01-29 08:28:31 +0530

    initial commit

---
+++ xMetal/xMetal/xMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  xMetalApp.swift
+//  xMetal
+//
+//  Created by Yasir on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct xMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
